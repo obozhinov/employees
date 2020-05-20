6ec84ef3ab54036e9eb5bd5a1b933e49cef8f5db
obozhinov <olya.bozhinov@gmail.com>
2020-05-20 12:43:56 +0300

    Autentication and authorization with keycloak

--- src/main/java/com/example/employees/SpringSecurityConfig.java
+++ src/main/java/com/example/employees/SpringSecurityConfig.java
@@ -1,55 +1,60 @@
 package com.example.employees;
 
-import org.springframework.context.annotation.Configuration;
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.keycloak.adapters.springsecurity.management.HttpSessionManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
-@Configuration
-public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+@KeycloakConfiguration
+public class SpringSecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    @Autowired
+    public void configureGlobal(
+            AuthenticationManagerBuilder auth) throws Exception {
 
-        auth.inMemoryAuthentication()
-                .withUser("admin").password("{noop}password").roles("USER","ADMIN")
-                .and()
-                .withUser("user").password("{noop}password").roles("USER");
-
+        KeycloakAuthenticationProvider keycloakAuthenticationProvider
+                = keycloakAuthenticationProvider();
+        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(
+                new SimpleAuthorityMapper());
+        auth.authenticationProvider(keycloakAuthenticationProvider);
     }
 
+    @Bean
+    public KeycloakSpringBootConfigResolver KeycloakConfigResolver() {
+        return new KeycloakSpringBootConfigResolver();
+    }
 
-    // Secure the endpoins with HTTP Basic authentication
+    @Bean
+    @ConditionalOnMissingBean(HttpSessionManager.class)
+    @Override
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(
+                new SessionRegistryImpl());
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
         http
-                //HTTP Basic authentication
-                .httpBasic()
-                .and()
+                .csrf().disable()
+                .antMatcher("*/**")
                 .authorizeRequests()
-                .antMatchers(HttpMethod.GET, "/employee/**").hasRole("USER")
+                .antMatchers(HttpMethod.GET, "/employee/**").hasAnyRole("USER", "ADMIN")
                 .antMatchers(HttpMethod.POST, "/employee").hasRole("ADMIN")
                 .antMatchers(HttpMethod.PUT, "/employee/**").hasRole("ADMIN")
                 .antMatchers(HttpMethod.DELETE, "/employee/**").hasRole("ADMIN")
-                .and()
-                .csrf().disable()
-                .formLogin().disable();
+                .anyRequest()
+                .permitAll();
     }
-
-//    public UserDetailsService userDetailsService() {
-//        //ok for demo
-//        User.UserBuilder users = User.withDefaultPasswordEncoder();
-//
-//        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-//        manager.createUser(users.username("user").password("password").roles("USER").build());
-//        manager.createUser(users.username("admin").password("password").roles("ADMIN").build());
-//        return manager;
-//    }
-
 }
